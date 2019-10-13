f5460768f239cb947457c51460f5f94121b6f886
himoritaaug <himorita.aug@gmail.com>
2019-10-13 15:16:39 +0900

    Initial commit

---
+++ src/main/java/com/himorita/hmbooking/HmbookingApplication.java
@@ -0,0 +1,13 @@
+package com.himorita.hmbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/himorita/hmbooking/HmbookingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.himorita.hmbooking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HmbookingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
